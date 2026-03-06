cfa1e45b244db542c0f44306e32a98d762830217
rebeccaberkheimer <rebeccaberkheimer15@gmail.com>
2026-03-06 11:27:53 -0500

    rating actually finished, brought back old repo methods

--- src/main/java/com/commonwealthu/tutor_scheduler/repository/RatingRepository.java
+++ src/main/java/com/commonwealthu/tutor_scheduler/repository/RatingRepository.java
@@ -5,12 +5,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
-    // Returns a list of every rating for a tutor, accounts for multiple ratings (past methods didn't do that,
-    // not sure how I didn't realize they would need to be lists)
-    @Query("SELECT r FROM Rating r WHERE r.tutor.tutorID = :tutorID")
-    List<Rating> findAllRatings(@Param("tutorID") String tutorID);
+    // Updated to bring old methods back, I thought they were all incorrect because of an error saying multiple
+    // numbers returned, but it was really just the overall because there was not an avg function on the outside
+
+    @Query("SELECT AVG(r.communication) FROM Rating r where r.tutor.tutorID = :tutorID")
+    Double findAvgComm(@Param("tutorID") String tutorID);
+
+    @Query("SELECT AVG(r.approach) FROM Rating r where r.tutor.tutorID = :tutorID")
+    Double findAvgApproach(@Param("tutorID") String tutorID);
+
+    @Query("SELECT AVG(r.effLearn) FROM Rating r where r.tutor.tutorID = :tutorID")
+    Double findAvgEffLearn(@Param("tutorID") String tutorID);
+
+    @Query("SELECT AVG(r.helpfulness) FROM Rating r where r.tutor.tutorID = :tutorID")
+    Double findAvgHelp(@Param("tutorID") String tutorID);
+
+    @Query("SELECT AVG((r.communication + r.approach + r.effLearn + r.helpfulness) / 4.0) " +
+            "FROM Rating r WHERE r.tutor.tutorID = :tutorID")
+    Double findAvgOverall(@Param("tutorID") String tutorID);
 
 }
--- src/main/java/com/commonwealthu/tutor_scheduler/service/RatingService.java
+++ src/main/java/com/commonwealthu/tutor_scheduler/service/RatingService.java
@@ -7,7 +7,6 @@ import com.commonwealthu.tutor_scheduler.repository.TutorRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
 
 @Service
 public class RatingService {
@@ -29,30 +28,12 @@ public class RatingService {
 
     public HashMap<String, Double> getAllRatings(String tutorID) {
         HashMap<String, Double> averages = new HashMap<>();
-        List<Rating> ratings = ratingRepo.findAllRatings(tutorID);
-        double comm = 0, approach = 0, effLearn = 0, help = 0;
-        for (Rating r : ratings) {
-            comm += r.getCommunication();
-            approach += r.getApproach();
-            effLearn += r.getEffLearn();
-            help += r.getHelpfulness();
-        }
-
-        double avgComm = comm / ratings.size();
-        double avgApproach = approach / ratings.size();
-        double avgEffLearn = effLearn / ratings.size();
-        double avgHelp = help / ratings.size();
-
-        double avgOverall = (avgComm + avgApproach + avgEffLearn + avgHelp) / 4;
-
-        averages.put("comm", avgComm);
-        averages.put("approach", avgApproach);
-        averages.put("effLearn", avgEffLearn);
-        averages.put("help", avgHelp);
-        averages.put("overall", avgOverall);
-
+        averages.put("comm", ratingRepo.findAvgComm(tutorID));
+        averages.put("approach", ratingRepo.findAvgApproach(tutorID));
+        averages.put("effLearn", ratingRepo.findAvgEffLearn(tutorID));
+        averages.put("help", ratingRepo.findAvgHelp(tutorID));
+        averages.put("overall", ratingRepo.findAvgOverall(tutorID));
         return averages;
     }
-
 
 }
