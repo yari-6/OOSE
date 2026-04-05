88583d6f5f2307b306200d5ad34b7e4d7855f5a5
ggilberg <ggilberg224@gmail.com>
2026-04-04 22:38:18 -0400

    add thymeleaf links to schedule display pages.

--- src/main/java/com/commonwealthu/tutor_scheduler/controller/SessionController.java
+++ src/main/java/com/commonwealthu/tutor_scheduler/controller/SessionController.java
@@ -18,7 +18,7 @@ public class SessionController {
     @GetMapping("/schedules/SI")
     public String siSchedule(Model model) {
         model.addAttribute("SIschedule", sessionService.getSessionsByType("SI"));
-        return "si-schedule";
+        return "SI Schedule";
     }
 
     @GetMapping("schedules/drop-in")
